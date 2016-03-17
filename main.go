a77ce616d49c36af07705d958b02d7576b2eb95c
Shinsuke Otsuki <otsuki.shinsuke@gmail.com>
2016-03-18 08:56:02 +0900

    Add the command line argument, asin

--- main.go
+++ main.go
@@ -1,14 +1,15 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 
 	"github.com/0tsuki/amazon/jp"
 )
 
-func AmazonScrape() {
-	p, err := jp.NewProduct("B019GNUT0C")
+func AmazonScrape(asin string) {
+	p, err := jp.NewProduct(asin)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -17,5 +18,7 @@ func AmazonScrape() {
 }
 
 func main() {
-	AmazonScrape()
+	var asin = flag.String("asin", "", "ID of an amazon product to find")
+	flag.Parse()
+	AmazonScrape(*asin)
 }
